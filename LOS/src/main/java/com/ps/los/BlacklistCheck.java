053cfeae3405db70f5a6f74b5a21fbc47e611e88
admin <>
2015-12-10 15:09:46 +0530

    {/LOS/src/main/java/com/ps/los/BlacklistCheck.java}

--- LOS/src/main/java/com/ps/los/BlacklistCheck.java
+++ LOS/src/main/java/com/ps/los/BlacklistCheck.java
@@ -10,7 +10,7 @@ public class BlacklistCheck implements java.io.Serializable
    static final long serialVersionUID = 1L;
 
    @org.kie.api.definition.type.Label(value = "Blacklist Report")
-   private java.lang.String blackListReportDocument;
+   private org.jbpm.document.Document blackListReportDocument;
    @org.kie.api.definition.type.Label(value = "Central Bank Blackliist Check")
    private java.lang.String centralBankBlackliistCheck;
    @org.kie.api.definition.type.Label(value = "Amount")
@@ -20,13 +20,13 @@ public class BlacklistCheck implements java.io.Serializable
    {
    }
 
-   public java.lang.String getBlackListReportDocument()
+   public org.jbpm.document.Document getBlackListReportDocument()
    {
       return this.blackListReportDocument;
    }
 
    public void setBlackListReportDocument(
-         java.lang.String blackListReportDocument)
+         org.jbpm.document.Document blackListReportDocument)
    {
       this.blackListReportDocument = blackListReportDocument;
    }
@@ -52,7 +52,7 @@ public class BlacklistCheck implements java.io.Serializable
       this.amount = amount;
    }
 
-   public BlacklistCheck(java.lang.String blackListReportDocument,
+   public BlacklistCheck(org.jbpm.document.Document blackListReportDocument,
          java.lang.String centralBankBlackliistCheck, java.lang.Double amount)
    {
       this.blackListReportDocument = blackListReportDocument;
